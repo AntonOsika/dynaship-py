c78a1292e03a7f58faa670eeb5e2e23b7d17ece0
John Häggkvist <john.haggkvist@gmail.com>
2013-11-03 12:22:41 +0100

    Find most probable random

--- shipsinker.py
+++ shipsinker.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import random
+import random, copy
 from gameboard.board import Board
 from gameboard.coordinate import Coordinate
 
@@ -11,7 +11,7 @@ class ShipSinker:
         if (intelligent_move):
             return intelligent_move
         else:
-            return self.getRandom(board)
+            return self.getIntelligentRandom(board)
 
     def getIntelligentMove(self, board):
         for y in range(board.size()):
@@ -36,6 +36,39 @@ class ShipSinker:
                             return nextMove        
         return self.pickNeighbour(board, x, y)
 
+    def weightBoard(self, board):
+        weights = []
+        for y in range(board.size()):
+            weights.append([])
+            for x in range(board.size()):
+                weights[y].append(0)
+        
+        for y in range(board.size()):
+            for x in range(board.size()):
+                for ship in board.ships():
+                    self._tryPlaceShipSouth(board, weights, ship, x, y)
+                    self._tryPlaceShipEast(board, weights, ship, x, y)
+
+        return weights
+        
+    def _tryPlaceShipSouth(self, board, weights, ship, x, y):
+        for lookForY in range(y, y + ship["length"]):
+            if (lookForY == board.size()):
+                return
+            if (self.get(board, x, lookForY)):
+                return
+        for shipY in range(y, y + ship["length"]):
+            weights[x][shipY] += 1
+
+    def _tryPlaceShipEast(self, board, weights, ship, x, y):
+        for lookForX in range(x, x + ship["length"]):
+            if (lookForX == board.size()):
+                return
+            if (self.get(board, lookForX, y)):
+                return
+        for shipX in range(x, x + ship["length"]):
+            weights[shipX][y] += 1
+
     def pickNeighbour(self, board, x, y):
         potential = []
         for neighbourX in [x-1, x, x+1]:
@@ -46,7 +79,6 @@ class ShipSinker:
                     and not self.get(board, neighbourX, neighbourY)):
                     potential.append(Coordinate(neighbourX, neighbourY))
         return potential.pop(random.randrange(len(potential)))
-
 
     def pickNextMoveWhenWeKnowTwoSeaworthy(self, board, x1, y1, x2, y2):
         potential = []
@@ -86,12 +118,31 @@ class ShipSinker:
                 continue
             return None
 
-    def getRandom(self, board):
+    def getIntelligentRandom(self, board):
+        weights = self.weightBoard(board)
+
+        positions = self._getTopPositionsFromWeigths(weights)
+        if (positions and len(positions) >= 1):
+            return positions[random.randrange(len(positions))]
+
         while True:
             x = random.randrange(board.size())
             y = random.randrange(board.size())
             if (not self.get(board, x, y) and self._canActuallyContainAliveShip(board, x, y)):
                 return Coordinate(x, y)
+
+    def _getTopPositionsFromWeigths(self, weights):
+        top = 0
+        positions = []
+        for y in range(len(weights)):
+            for x in range(len(weights[y])):
+                if (weights[x][y] > top):
+                    top = weights[x][y]
+                    positions = []
+                
+                if (weights[x][y] == top):
+                    positions.append(Coordinate(x, y))
+        return positions
 
     def _canActuallyContainAliveShip(self, board, x, y):
         smallestShip = 5
